b881d1eba5896293b41688ff03e506424df1ffc8
Alessio Casablanca <159435390+Alessio76184@users.noreply.github.com>
2024-02-21 18:22:26 +0200

    Update recursive_loop.py

--- recursive_loop.py
+++ recursive_loop.py
@@ -1,16 +1,19 @@
 NUMBER_OF_DISKS = 5
+##hanoi puzzel usually has all disks stacked in decending order
 A = list(range(NUMBER_OF_DISKS, 0, -1))
 B = []
 C = []
 
+## n = number of disks
 def move(n, source, auxiliary, target):
+    ##ends the recursion
     if n <= 0:
         return
     
-    # move n - 1 disks from source to auxiliary, so they are out of the way
+    # move n - 1 (checking the disk number until n<= 0) disks from source to auxiliary, so they are out of the way
     move(n - 1, source, target, auxiliary)
     
-    # move the nth disk from source to target
+    # move the nth disk from source to target | source.pop() removes the disk from source and target.append adds this digarded disk to target.
     target.append(source.pop())
     
     # display our progress
